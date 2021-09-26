3130acf1068f1b0116b621e1f891ee5bf419050a
jake-garnier <53188176+jake-garnier@users.noreply.github.com>
2021-09-26 14:49:23 -0700

    added python template

--- flaskw/testingTemplates/pythonTestingTemplateLambda.py
+++ flaskw/testingTemplates/pythonTestingTemplateLambda.py
@@ -1,18 +1,5 @@
-import json
-from importlib import import_module
-import test_test
-import importlib
+import unittest
+
 
 def lambda_handler(event, context):
-    # TODO implement
-
-    # import_module('test')
-
-    importlib.reload(test_test)
-
-    return test_test.main('arg1 arg2 arg3 arg4'.split(' '))
-    
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    unittest.main(module='test', exit=False)
